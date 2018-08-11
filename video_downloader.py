5d04d03e789c7e2a0d0f06df1f73649e84342fea
uthum <muthumani1999994@gmail.com>
2018-08-11 17:05:48 +0530

    Update video_downloader.py

--- video_downloader.py
+++ video_downloader.py
@@ -7,6 +7,6 @@ vid=video.streams.all()
 for i in video.streams.all():
 	print(str(opt)+". "+str(i))
 	opt=opt+1
-vid=vid[int(input("enter num:"))]
+vid=vid[int(input("enter number:"))]
 path=input("Enter the path:")
 vid.download(path)
